340bcde74184eff45ce30a8376d9dffb7d0e0623
eunji Lee <eunjitech@gmail.com>
2023-05-16 17:24:21 +0900

    self: 카테고리 화면에 띄우기

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
+import { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Dimensions,
+  SafeAreaView,
+  Platform,
+} from "react-native";
+import Constants from "expo-constants";
+import { CATEGORIES, MEALS } from "./data/dummy-data";
 
 export default function App() {
+  const [statusBarHeight, setStatusBarHeight] = useState(0);
+
+  useEffect(() => {
+    setStatusBarHeight(Constants.statusBarHeight);
+  }, []);
+
+  const renderItem = ({ item }) => {
+    return (
+      <View style={[styles.itemContainer, { backgroundColor: item.color }]}>
+        <Text style={styles.itemText}>{item.title}</Text>
+      </View>
+    );
+  };
+
+  const { width, height } = Dimensions.get("window");
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      paddingTop: Platform.OS === "android" ? statusBarHeight : 0,
+      backgroundColor: "#eee",
+    },
+    itemsContainer: {
+      flexDirection: "row",
+      flexWrap: "wrap",
+      justifyContent: "space-around",
+    },
+    itemContainer: {
+      flex: 1,
+      width: width / 2,
+      height: width / 2,
+      alignItems: "center",
+      justifyContent: "center",
+      flexDirection: "row",
+    },
+    itemText: {
+      fontSize: width > 500 ? 22 : 18,
+      fontWeight: "bold",
+      color: "#eee",
+      textShadowColor: "#222",
+      textShadowRadius: 4,
+    },
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Food App</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={CATEGORIES}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+          contentContainerStyle={styles.itemsContainer}
+        />
+      </SafeAreaView>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
